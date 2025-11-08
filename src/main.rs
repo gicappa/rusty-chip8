b411be5be1c693628cf60f09561e312637f191d3
Gian Carlo Pace <giancarlo.pace@hyland.com>
2025-11-08 01:47:28 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,26 @@ mod cpu_op_04;
 #[allow(dead_code)]
 mod debug_cli;
 mod clock;
-mod cpu_engine;
+mod cpu_core;
 
 use crate::cpu::Cpu;
 use crate::gpu::Gpu;
 
 use crate::clock::Clock;
-use crate::cpu_engine::CpuEngine;
+use crate::cpu_core::CpuCore;
 
 fn main() {
     let mut cpu = Cpu::new();
-    let mut cpu_engine = CpuEngine::new(&mut cpu);
+    let mut core = CpuCore::new(&mut cpu);
     let mut gpu = Gpu::new();
     let mut clock = Clock::new();
 
     loop {
         clock.start();
-        cpu_engine.clk();
-        gpu.clk(cpu_engine.cpu.vram, cpu_engine.draw_flag());
+        core.clk();
+        gpu.clk(
+            core.cpu.vram,
+            core.draw_flag());
         clock.stop_and_wait();
     }
 
