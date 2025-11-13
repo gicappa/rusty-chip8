023af06d6c940497b3a73f845b7dc5e729e7b64f
Gian Carlo Pace <giancarlo.pace@hyland.com>
2025-11-13 15:12:09 +0100

    added support to rom file as an argument

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,42 @@ mod gpu;
 mod config;
 mod cpu;
 mod cpu_opcode;
+#[allow(dead_code)]
 mod cpu_debugger;
 mod clock;
 mod cpu_core;
 
+use clap::Parser;
 use crate::clock::Clock;
 use crate::cpu::Cpu;
 use crate::cpu_core::CpuCore;
-use crate::gpu::Gpu;
 use crate::cpu_debugger::CpuDebugger;
+use crate::gpu::Gpu;
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "oxide",
+    version,
+    about = "A Chip8 Emulator written in Rust"
+)]
+struct Args {
+    rom_file: Option<String>,
+}
 
 fn main() {
+    let args = Args::parse();
+
     let mut cpu = Cpu::new();
     let mut core = CpuCore::new();
     let mut gpu = Gpu::new("0xID8");
     let mut clock = Clock::new();
     let mut cpu_debugger = CpuDebugger::new();
 
-    core
-        .load_rom(&mut cpu, "tests/6-keypad.ch8")
-        .expect("File not found or not readable");
+    args.rom_file.map(|r| {
+        core
+            .load_rom(&mut cpu, &r)
+            .expect(&format!("File {} not found or not readable", &r));
+    });
 
     if !cpu.panic {
         gpu.panic(&mut cpu);
