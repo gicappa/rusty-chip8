ca76379075b4640adbb324ce4bd0f9fbbec536ca
Gian Carlo Pace <giancarlo.pace@hyland.com>
2025-11-16 16:46:44 +0100

    winit instead of minifb

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ mod cpu_opcode;
 mod cpu_debugger;
 mod clock;
 mod cpu_core;
+mod video_input;
 
 use clap::Parser;
 use crate::clock::Clock;
 use crate::cpu::Cpu;
 use crate::cpu_core::CpuCore;
 use crate::cpu_debugger::CpuDebugger;
-use crate::gpu::Gpu;
+// use crate::gpu::Gpu;
+use crate::video_input::VideoInput;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -29,9 +31,10 @@ fn main() {
 
     let mut cpu = Cpu::new();
     let mut core = CpuCore::new();
-    let mut gpu = Gpu::new("0xID8");
+    // let mut gpu = Gpu::new("0xID8");
     let mut clock = Clock::new();
     let mut cpu_debugger = CpuDebugger::new();
+    let _ = VideoInput::start();
 
     args.rom_file.map(|r| {
         core
@@ -40,13 +43,13 @@ fn main() {
     });
 
     if !cpu.panic {
-        gpu.panic(&mut cpu);
+        // gpu.panic(&mut cpu);
     }
 
     while cpu.running {
         clock.start();
         core.tick(&mut cpu);
-        gpu.tick(&mut cpu);
+        // gpu.tick(&mut cpu);
         // could be used in a thread to avoid using CPU time to draw cpu status
         cpu_debugger.tick(&mut cpu).unwrap();
         clock.stop_and_wait();
---
+++ src/video_input.rs
@@ -0,0 +1,106 @@
+use crate::config::{H, W};
+use pixels::{Pixels, SurfaceTexture};
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, /*ControlFlow,*/ EventLoop},
+    window::Window,
+};
+
+const WIDTH: u32 = W as u32;
+const HEIGHT: u32 = H as u32;
+
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Default)]
+pub struct VideoInput {
+    window: Option<&'static Window>,
+    pixels: Option<Pixels<'static>>,
+}
+
+impl VideoInput {
+    pub fn start() -> Result<()> {
+        let event_loop = EventLoop::new()?;
+        let mut app = VideoInput::default();
+        event_loop.run_app(&mut app)?;
+
+        Ok(())
+    }
+}
+
+impl ApplicationHandler for VideoInput {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop.create_window(
+            Window::default_attributes()
+                .with_title("0xID8")
+                .with_resizable(false)
+                .with_inner_size(winit::dpi::LogicalSize::new(640.0, 320.0))
+        ).unwrap();
+
+        let size = window.inner_size();
+        let window_ref: &'static Window = Box::leak(Box::new(window));
+        let surface = SurfaceTexture::new(size.width, size.height, window_ref);
+        let pixels = Pixels::new(WIDTH, HEIGHT, surface).unwrap();
+
+        self.window = Some(window_ref);
+        self.pixels = Some(pixels);
+
+        let scale_factor = window_ref.scale_factor();
+        println!("Scale factor : {}", scale_factor);
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _: winit::window::WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+
+            WindowEvent::RedrawRequested => {
+                if let Some(pixels) = &mut self.pixels {
+                    let frame = pixels.frame_mut();
+
+                    for spot in frame.chunks_exact_mut(4) {
+                        spot[0] = 0x3a; // R
+                        spot[1] = 0x23; // G
+                        spot[2] = 0x17; // B
+                        spot[3] = 0xFF; // A
+                    }
+
+                    put_pixel(frame, 10, 5, 0xFF, 0xFF, 0xFF, 0xFF);
+
+                    pixels.render().unwrap();
+                }
+
+                if let Some(window) = &self.window {
+                    window.request_redraw();
+                }
+            }
+
+            _ => {}
+        }
+    }
+
+    fn about_to_wait(&mut self, _: &ActiveEventLoop) {
+        self.window.expect("Bug - Window should exist").request_redraw();
+    }
+
+    //	#b07154	(176,113,84)
+    // #f4decb	(244,222,203)
+    // #a87e62	(168,126,98)
+    // #3a2317	(58,35,23)
+    // #311f13	(49,31,19)
+}
+fn put_pixel(frame: &mut [u8], x: u32, y: u32, r: u8, g: u8, b: u8, a: u8) {
+    // Make sure we don't go out of bounds
+    if x >= WIDTH || y >= HEIGHT {
+        return;
+    }
+
+    let idx = ((y * WIDTH + x) * 4) as usize;
+
+    frame[idx    ] = r; // R
+    frame[idx + 1] = g; // G
+    frame[idx + 2] = b; // B
+    frame[idx + 3] = a; // A
+}
